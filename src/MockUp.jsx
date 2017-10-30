229a2c6dd741e71aa4dfc0ec5557246e96a6b543
Roadagain <ringoh72@gmail.com>
2017-10-30 13:39:35 +0900

    Reactでモックアップ用のページを作成

---
+++ src/MockUp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class MockUp extends React.Component {
+    render() {
+        return <h1>hydroponics-web</h1>
+    }
+}
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MockUp from './MockUp';
+
+ReactDOM.render(<MockUp></MockUp>, document.getElementById('container'));

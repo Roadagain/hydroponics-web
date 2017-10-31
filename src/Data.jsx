7f59f023cc8085b185bb522daa2f85b11efb4ad6
Roadagain <ringoh72@gmail.com>
2017-10-31 13:54:28 +0900

    テーブルヘッダは中央寄せ、各セルの中身は右寄せ

--- src/Data.jsx
+++ src/Data.jsx
@@ -7,21 +7,21 @@ export default class Data extends React.Component {
         for (let i = 0; i < 31; ++i){
             const dateStr = '2017/01/' + (i < 10 ? '0' : '') + (i + 1);
             data.push(<tr>
-                <th>{dateStr}</th>
-                <th>{i + 1}</th>
-                <th>23</th>
-                <th>60</th>
-                <th>23</th>
+                <th className="text-right">{dateStr}</th>
+                <th className="text-right">{i + 1}</th>
+                <th className="text-right">23</th>
+                <th className="text-right">60</th>
+                <th className="text-right">23</th>
             </tr>);
         }
         return <Table striped bordered responsive>
             <thead>
                 <tr>
-                    <th>日時</th>
-                    <th>大きさ[cm]</th>
-                    <th>気温[°C]</th>
-                    <th>湿度[%]</th>
-                    <th>水温[°C]</th>
+                    <th className="text-center">日時</th>
+                    <th className="text-center">大きさ[cm]</th>
+                    <th className="text-center">気温[°C]</th>
+                    <th className="text-center">湿度[%]</th>
+                    <th className="text-center">水温[°C]</th>
                 </tr>
             </thead>
             <tbody>{data}</tbody>

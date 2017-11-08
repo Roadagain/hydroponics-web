fbc3c652c3c3642ffd204bf18dd2d7cde2d0051d
Roadagain <ringoh72@gmail.com>
2017-11-08 17:47:29 +0900

    react-routingでルーティングする

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MockUp from './MockUp';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-ReactDOM.render(<MockUp></MockUp>, document.getElementById('container'));
+ReactDOM.render(<BrowserRouter>
+    <div>
+        <Route path="/" component={MockUp}/>
+    </div>
+</BrowserRouter>, document.getElementById('container'));
+//ReactDOM.render(<MockUp></MockUp>, document.getElementById('container'));

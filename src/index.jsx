1178971565902733884370724ac02dc28dd38820
Roadagain <ringoh72@gmail.com>
2017-12-04 22:26:15 +0900

    ホーム画面はHomeコンポーネントを表示

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navigation from './Navigation';
+import Home from './Home';
 import Measured from './Measured';
 import Analysed from './Analysed';
 import {BrowserRouter, Route} from 'react-router-dom';
@@ -8,9 +9,10 @@ import {BrowserRouter, Route} from 'react-router-dom';
 ReactDOM.render(
     <BrowserRouter>
         <div>
-            <Route path="/" component={Navigation}/>
-            <Route path="/measured" component={Measured}/>
-            <Route path="/analysed" component={Analysed}/>
+            <Navigation/>
+            <Route exact path="/" component={Home} />
+            <Route path="/measured" component={Measured} />
+            <Route path="/analysed" component={Analysed} />
         </div>
     </BrowserRouter>,
     document.getElementById('container'));
